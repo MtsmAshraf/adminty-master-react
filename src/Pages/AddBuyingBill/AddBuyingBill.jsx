2fa529f9b089f2ada2b9885186bfa23c30b3cbb5
moatasim <mo32000a@gmail.com>
2024-04-22 02:11:32 +0200

    added drag-area to AddBuyingBill

--- src/Pages/AddBuyingBill/AddBuyingBill.jsx
+++ src/Pages/AddBuyingBill/AddBuyingBill.jsx
@@ -7,6 +7,42 @@ const AddBuyingBill = () => {
         e.preventDefault();
         window.location.replace('/invoice-list');
     }
+    useEffect(() => {
+        console.log("mounted")
+        let imgView = document.getElementById("img-view");
+        let uploadBillInput = document.getElementById("upload-bill");
+        let dragArea = document.getElementById("drag-area");
+    
+        let viewImg = () => {
+            console.log(uploadBillInput.files[0])
+            if(uploadBillInput.files[0] && uploadBillInput.files[0].type !== "application/pdf"){
+                let imgLink = URL.createObjectURL(uploadBillInput.files[0]);
+                imgView.style.backgroundImage = `url(${imgLink})`;
+                imgView.textContent = ""
+            }else if(uploadBillInput.files[0] && uploadBillInput.files[0].type === "application/pdf"){
+                console.log(uploadBillInput.files[0])
+                imgView.style.backgroundImage = "";
+                imgView.textContent = uploadBillInput.files[0].name
+            }else if(uploadBillInput.files.length === 0 ){
+                console.log("aiosdjoaidh") 
+                imgView.style.backgroundImage = "";
+                imgView.innerHTML = `
+                    <h5 style="color: #666;">اسحب الملفات هنا</h5>
+                    <span style="color: #999;">أو</span>
+                    <h5 style="color: #666;">اضغط لرفع المرفقات</h5>
+                `
+            }
+        }
+        uploadBillInput.addEventListener("change", viewImg)
+        dragArea.addEventListener("dragover",(e) => {
+            e.preventDefault();
+        })
+        dragArea.addEventListener("drop",(e) => {
+            e.preventDefault();
+            uploadBillInput.files = e.dataTransfer.files;
+            viewImg();
+        })
+    })
     let [valueOne, setValueOne] = useState(0) 
     let [quantityOne, setQuantityOne] = useState(1) 
     let [valueTwo, setValueTwo] = useState(0) 
@@ -715,6 +751,17 @@ const AddBuyingBill = () => {
                         </td>
                       </tr>
                     </table>
+                  </div>
+                  <div className="form-group row text-center" id="drag-area">
+                      <label className="col-sm-12 col-form-label" htmlFor="upload-bill">أضف المرفقات</label>
+                      <div className="col-sm-12">
+                          <label id="img-view" className="col-sm-12 col-form-label" htmlFor="upload-bill">
+                              <h5>اسحب الملفات هنا</h5>
+                              <span>أو</span>
+                              <h5>اضغط لرفع المرفقات</h5>
+                          </label>
+                          <input type="file" id="upload-bill" className="form-control" hidden/>
+                      </div>
                   </div>
                   <div className="btn-pair">
                           <input type="submit" value="حفظ"  onClick={(e) => {redirectFunction(e)}}/>
