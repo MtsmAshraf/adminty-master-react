6145897fe3c24d66261cdeb58dfbfbe01abc994f
moatasim <mo32000a@gmail.com>
2024-04-23 01:06:27 +0200

    added adding msgs to the chat body

--- src/Pages/InvoiceList/InvoiceList.jsx
+++ src/Pages/InvoiceList/InvoiceList.jsx
@@ -10,8 +10,46 @@ const InvoiceList = () => {
     let [paidStatus, setPaidStatus] = useState("full")
     let [paidStatusSal, setPaidStatusSal] = useState("partially")
     let [delivered, setDelivered] = useState(true)
+    let formHandler = () => {
+        let chatBody = document.querySelector("#chat-body")
+        let messageValue = document.querySelector("#message").value;
+        let messageP = document.createElement("p")
+        let message = document.createElement("div")
+        messageP.innerHTML = messageValue
+        message.appendChild(messageP)
+        let messageSpan = document.createElement("span")
+        messageSpan.innerHTML = "10:31 PM"
+        message.setAttribute("message-src", "user")
+        message.appendChild(messageSpan)
+        message.style.cssText = `
+            margin-bottom: 10px;
+            text-align: right;
+            width: 100%;
+        `
+        messageP.style.cssText = `
+            padding: 10px 20px;
+            margin-bottom: 5px;
+            text-align: right;
+            max-width: 60%;
+            width: fit-content;
+            line-height: 1.4;
+            background-color: #ddd;
+            border-radius: var(--main-border-radius) 0px calc(var(--main-border-radius) + 10px) var(--main-border-radius);
+            background-color: var(--main-color);
+            color: #fff;
+            margin-left: auto;
+            margin-right: 0px;
+        `
+        messageSpan.style.cssText = `
+            color: #777;
+            font-size: 14px;
+        `
+        chatBody.appendChild(message)
+        console.log(message)
+    }
 
     useEffect(() => {
+    
         let leftHeaderLis = document.querySelectorAll("#left-header li")
         let leftToggledParts = document.querySelectorAll("#left > *:not(#left-header)")
         leftHeaderLis.forEach((li, index) => {
@@ -280,7 +318,7 @@ const InvoiceList = () => {
                                 اسم المستخدم
                             </h5>
                         </div>
-                        <div className={styles.chatBody}>
+                        <div className={styles.chatBody} id="chat-body">
                             <div className={styles.message} message-src="client">
                                 <p>
                                     مرحبا! كيف حالك؟ 
@@ -316,16 +354,16 @@ const InvoiceList = () => {
                                 <span>10:43 PM</span>
                             </div>
                         </div>
-                        <div className={styles.chatForm}>
+                        <form className={styles.chatForm}>
                             <div className={styles.inputWrapper}>
-                                <textarea name="" id="" placeholder="أكتب رسالتك هنا..." ></textarea>
+                                <textarea name="" id="message" placeholder="أكتب رسالتك هنا..." ></textarea>
                             </div>
                             <div className={styles.btnWrapper}>
-                                <button type="button" className={styles.btn}>
+                                <button type="button" className={styles.btn} onClick={formHandler}>
                                     <FontAwesomeIcon icon={faPaperPlane} />
                                 </button>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
                 <div className={styles.trackingWrapper}>
