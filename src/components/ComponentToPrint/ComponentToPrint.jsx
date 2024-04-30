fbc08db5999f08199dd938d3607b2983f60d39ae
moatasim <mo32000a@gmail.com>
2024-04-30 06:26:28 +0300

    added the first version of the printed table

--- src/components/ComponentToPrint/ComponentToPrint.jsx
+++ src/components/ComponentToPrint/ComponentToPrint.jsx
@@ -75,37 +75,142 @@ class ComponentToPrint extends React.Component {
                 </th>
               </thead>
               <tr className={styles.headRow}>
-                <td>
-                  رقم البوليصة
+                <td colSpan={2}>
+                  رقم البيان
+                </td>
+                <td colSpan={2}>
+                  التاريخ
                 </td>
                 <td>
-                  اسم المستورد
+                  المبلغ (SAR)
                 </td>
-                <td>
-                  اسم المصدر
+                <td colSpan={2}>
+                  الرقم الموحد
                 </td>
-                <td>
-                  بلد القدوم
-                </td>
-                <td>
-                  بلد الوصول
+                <td colSpan={2}>
+                  حالة السداد
                 </td>
               </tr>
               <tr>
-                <td>
+                <td colSpan={2}>
                   067-65465768
                 </td>
-                <td>
-                  اسم المستورد
+                <td colSpan={2}>
+                  25 - 7 - 2024
                 </td>
                 <td>
-                  اسم المصدر
+                  187523
+                </td>
+                <td colSpan={2}>
+                  067-65465768
+                </td>
+                <td colSpan={2}>
+                  مدفوع
+                </td>
+              </tr>
+              <thead>
+                <th colSpan={9}>
+                  بيانات سداد سال
+                </th>
+              </thead>
+              <tr className={styles.headRow}>
+                <td colSpan={2}>
+                  رقم سداد سال
+                </td>
+                <td colSpan={2}>
+                  التاريخ
                 </td>
                 <td>
-                  United States of America
+                  المبلغ (SAR)
+                </td>
+                <td colSpan={2}>
+                  عدد الطرود
+                </td>
+                <td colSpan={2}>
+                  حالة السداد
+                </td>
+              </tr>
+              <tr>
+                <td colSpan={2}>
+                  067-65465768
+                </td>
+                <td colSpan={2}>
+                  25 - 7 - 2024
                 </td>
                 <td>
-                  Saudi Arabia
+                  187523
+                </td>
+                <td colSpan={2}>
+                  32
+                </td>
+                <td colSpan={2}>
+                  مدفوع
+                </td>
+              </tr>
+              <thead>
+                <th colSpan={9}>
+                  بيانات عملية النقل
+                </th>
+              </thead>
+              <tr className={styles.headRow}>
+                <td colSpan={3}>
+                  موقع التنزيل
+                </td>
+                <td colSpan={2}>
+                  اسم المستلم
+                </td>
+                <td colSpan={2}>
+                  رقم الجوال
+                </td>
+                <td colSpan={2}>
+                  عدد الطرود
+                </td>
+              </tr>
+              <tr>
+                <td colSpan={3}>
+                  067-65465768
+                </td>
+                <td colSpan={2}>
+                  25 - 7 - 2024
+                </td>
+                <td colSpan={2}>
+                  96034234251
+                </td>
+                <td colSpan={2}>
+                  10
+                </td>
+              </tr>
+              <thead>
+                <th colSpan={9}>
+                  تفاصيل البضاعة
+                </th>
+              </thead>
+              <tr className={styles.headRow}>
+                <td colSpan={3}>
+                  رقم البوليصة
+                </td>
+                <td colSpan={2}>
+                  اسم الشركة
+                </td>
+                <td colSpan={2}>
+                  عدد الطرود
+                </td>
+                <td colSpan={2}>
+                  اسم السائق المستلم
+                </td>
+              </tr>
+              <tr>
+                <td colSpan={3}>
+                  067-65465768
+                </td>
+                <td colSpan={2}>
+                  اسم الشركة
+                </td>
+                <td colSpan={2}>
+                  13
+                </td>
+                <td colSpan={2}>
+                  اسم السائق المستلم
                 </td>
               </tr>
             </table>
