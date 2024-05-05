8af65cfd03bbc6c2db8a2941f93d1b24ed5ad0fa
moatasim <mo32000a@gmail.com>
2024-05-05 17:46:48 +0300

    added contract page

--- src/Pages/Contract/Contract.jsx
+++ src/Pages/Contract/Contract.jsx
@@ -1,8 +1,16 @@
+import Breadcumbs from "../../components/Breadcumbs/Breadcumbs"
+import Card from "../../components/Card/Card"
 import styles from "./contract.module.css"
+import contract from "../../assets/files/235-53428896.pdf"
 
 const Contract = () => {
   return (
-    <div className={styles.contract}>Contract</div>
+    <div className={styles.contract}>
+        <Breadcumbs pageName={"تفاصيل العقد"}></Breadcumbs>
+        <Card>
+            <iframe src={contract}></iframe>
+        </Card>
+    </div>
   )
 }
 
