e48f38aa719b86e0ed77ab6e81d20a7395d5587d
moatasim <mo32000a@gmail.com>
2024-05-08 11:59:17 +0300

    trying to fetch data in billDetails.jsx

--- src/Pages/BillDetails/BillDetails.jsx
+++ src/Pages/BillDetails/BillDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "./bill-details.module.css"
 import Card from '../../components/Card/Card'
 import billPdf from "../../assets/files/235-53428896.pdf"
@@ -6,7 +6,15 @@ import { Link } from 'react-router-dom'
 import { faMoneyBills, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const BillDetails = () => {
+    var bills = []
     useEffect(() => {
+        const fetchData = async () => {
+            const request = await fetch("https://jsonplaceholder.typicode.com/users")
+            const data = await request.json()
+            bills = data
+            console.log(bills)
+        }
+        fetchData()
         let additionalBills = document.querySelectorAll("#additional-bills li")
         additionalBills.forEach(li => {
             li.onclick = () => {
@@ -16,7 +24,7 @@ const BillDetails = () => {
                 li.setAttribute("status", "active")
             }
         })
-    },[])
+    })
     return (
         <div className={styles.billDetails}>
             <div className={styles.additionalBills}>
@@ -32,50 +40,36 @@ const BillDetails = () => {
                         <h3>مصاريف إضافية</h3>
                     </div>
                     <ul id="additional-bills">
-                        <li  status="active">
+                        {bills}
+                        {
+                            bills.map((bill) => {
+                                return(
+                                    <li key={bill.id}>
+                                        <span>
+                                            <FontAwesomeIcon icon={faMoneyBills} />
+                                        </span>
+                                        <div className={styles.text}>
+                                            {/* <h4>مصاريف إضافية</h4> */}
+                                            <p>
+                                                {
+                                                    bill.name
+                                                }
+                                            </p>
+                                        </div>
+                                    </li>
+                                )
+                            })
+                        }
+                        {/* <li  status="active">
                             <span>
                                 <FontAwesomeIcon icon={faMoneyBills} />
                             </span>
                             <div className={styles.text}>
-                                {/* <h4>مصاريف إضافية</h4> */}
                                 <p>
                                     تفاصيل الفاتورة الإضافية
                                 </p>
                             </div>
-                        </li>
-                        <li >
-                            <span>
-                                <FontAwesomeIcon icon={faMoneyBills} />
-                            </span>
-                            <div className={styles.text}>
-                                {/* <h4>مصاريف إضافية</h4> */}
-                                <p>
-                                    تفاصيل الفاتورة الإضافية
-                                </p>
-                            </div>
-                        </li>
-                        <li >
-                            <span>
-                                <FontAwesomeIcon icon={faMoneyBills} />
-                            </span>
-                            <div className={styles.text}>
-                                {/* <h4>مصاريف إضافية</h4> */}
-                                <p>
-                                    تفاصيل الفاتورة الإضافية
-                                </p>
-                            </div>
-                        </li>
-                        <li >
-                            <span>
-                                <FontAwesomeIcon icon={faMoneyBills} />
-                            </span>
-                            <div className={styles.text}>
-                                {/* <h4>مصاريف إضافية</h4> */}
-                                <p>
-                                    تفاصيل الفاتورة الإضافية
-                                </p>
-                            </div>
-                        </li>
+                        </li> */}
                     </ul>
                 </Card>
                 
