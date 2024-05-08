9dd8ea6fcf356ebfeddc08a6ab0beeec9352fcdb
moatasim <mo32000a@gmail.com>
2024-05-08 14:30:12 +0300

    added fetchData and fetchSingleData in billDetails.jsx

--- src/Pages/BillDetails/BillDetails.jsx
+++ src/Pages/BillDetails/BillDetails.jsx
@@ -7,13 +7,28 @@ import { faMoneyBills, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const BillDetails = () => {
     let [bills, setBills] = useState()
+    let [billId, setBillId] = useState()
+    let [billName, setBillName] = useState()
+    let [billPrice, setBillPrice] = useState()
+    let [billDetails, setBillDetails] = useState()
+    const fetchData = async () => {
+        const request = await fetch("https://jsonplaceholder.typicode.com/users")
+        const data = await request.json()
+        setBills(data)
+        // setBillId(data[0].id)
+        // setBillName(data[0].name)
+        // setBillPrice(data[0].name)
+        // setBillDetails(data[0].name)
+    }
+    const fetchSingleData = async (userId) => {
+        const request = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+        const data = await request.json()
+        setBillId(data.id)
+        setBillName(data.name)
+        setBillPrice(data.name)
+        setBillDetails(data.name)
+    }
     useEffect(() => {
-        const fetchData = async () => {
-            const request = await fetch("https://jsonplaceholder.typicode.com/users")
-            const data = await request.json()
-            setBills(data)
-            console.log(bills)
-        }
         fetchData()
         let additionalBills = document.querySelectorAll("#additional-bills li")
         additionalBills.forEach(li => {
@@ -33,19 +48,21 @@ const BillDetails = () => {
                     <form action="">
                         <div>
                             <label htmlFor="">رقم الفاتورة</label>
-                            <input type="number" />
+                            <input value={billId} type="number" />
                         </div>
                         <div>
                             <label htmlFor="">اسم الفاتورة</label>
-                            <input type="text" />
+                            <input value={billName} type="text" />
                         </div>
                         <div>
                             <label htmlFor="">المبلغ</label>
-                            <input type="number" />
+                            <input value={billPrice} type="number" />
                         </div>
                         <div>
                             <label htmlFor="">تفاصيل الفاتورة</label>
-                            <textarea name="" id=""></textarea>
+                            <textarea name="" id="">
+                                {billName}
+                            </textarea>
                         </div>
                         <div className={styles.btns}>
                             <input type="submit" value="حفظ"/>
@@ -64,7 +81,7 @@ const BillDetails = () => {
                         {
                             bills && bills.map((bill) => {
                                 return(
-                                    <li key={bill.id}>
+                                    <li key={bill.id} onClick={() => {fetchSingleData(bill.id)}}>
                                         <span>
                                             <FontAwesomeIcon icon={faMoneyBills} />
                                         </span>
