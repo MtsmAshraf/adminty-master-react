e4bd5713b6d294e9f8c48da74f5ab6dc245fd191
moatasim <mo32000a@gmail.com>
2024-05-18 16:16:23 +0300

    added hideShowCols functionality in BillsList page

--- src/Pages/BillsList/BillsList.jsx
+++ src/Pages/BillsList/BillsList.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react"
 import Breadcumbs from "../../components/Breadcumbs/Breadcumbs"
 import Card from "../../components/Card/Card"
 import styles from "./bills-list.module.css"
 
 const BillsList = () => {
+    const [billId, setBillId] = useState(true)
+    const [clientName, setClientName] = useState(true)
+    const [clienLasttName, setClientLastName] = useState(true)
+    const [email, setEmail] = useState(true)
+
     return (
         <div className={styles.billsList}>
             <Breadcumbs pageName={"قائمة الشحنات"}></Breadcumbs>
@@ -14,19 +20,19 @@ const BillsList = () => {
                                 قائمة الشحنات
                             </h3>
                             <div>
-                                <input type="checkbox" name="hide-show-cols" id="bill-id" />
+                                <input type="checkbox" onChange={(e) => {console.log(setBillId(e.target.checked))}} checked={billId} name="hide-show-cols" id="bill-id" />
                                 <label htmlFor="">
                                     رقم البوليصة
                                 </label>
-                                <input type="checkbox" name="hide-show-cols" id="client-name" />
+                                <input type="checkbox" onChange={(e) => {console.log(setClientName(e.target.checked))}} checked={clientName} name="hide-show-cols" id="client-name" />
                                 <label htmlFor="">
                                     اسم العميل
                                 </label>
-                                <input type="checkbox" name="hide-show-cols" id="client-last-name" />
+                                <input type="checkbox" onChange={(e) => {console.log(setClientLastName(e.target.checked))}} checked={clienLasttName} name="hide-show-cols" id="client-last-name" />
                                 <label htmlFor="">
                                     last name
                                 </label>
-                                <input type="checkbox" name="hide-show-cols" id="email" />
+                                <input type="checkbox" onChange={(e) => {console.log(setEmail(e.target.checked))}} checked={email} name="hide-show-cols" id="email" />
                                 <label htmlFor="">
                                     الإيميل
                                 </label>
@@ -34,103 +40,77 @@ const BillsList = () => {
                         </div>
                         <table cellSpacing={0}>
                             <thead>
-                                <th>
+                                <th style={{ display:  billId ? "table-cell" : "none"}}>
                                     رقم البوليصة
                                 </th>
-                                <th>
+                                <th style={{ display: clientName ? "table-cell" : "none"}}>
                                     اسم العميل
                                 </th>
-                                <th>
+                                <th style={{ display: clienLasttName ? "table-cell" : "none"}}>
                                     last name
                                 </th>
-                                <th>
+                                <th style={{ display: email ? "table-cell" : "none"}}>
                                     الإيميل
                                 </th>
                             </thead>
-                            <tr>
-                                <td>
-                                    789-654987
-                                </td>
-                                <td>
-                                    اسم العميل
-                                </td>
-                                <td>
-                                    اسم العميل
-                                </td>
-                                <td>
-                                    client312402@example.com
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    789-654987
-                                </td>
-                                <td>
-                                    اسم العميل
-                                </td>
-                                <td>
-                                    اسم العميل
-                                </td>
-                                <td>
-                                    client312402@example.com
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    789-654987
-                                </td>
-                                <td>
-                                    اسم العميل
-                                </td>
-                                <td>
-                                    اسم العميل
-                                </td>
-                                <td>
-                                    client312402@example.com
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    789-654987
-                                </td>
-                                <td>
-                                    اسم العميل
-                                </td>
-                                <td>
-                                    اسم العميل
-                                </td>
-                                <td>
-                                    client312402@example.com
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    789-654987
-                                </td>
-                                <td>
-                                    اسم العميل
-                                </td>
-                                <td>
-                                    اسم العميل
-                                </td>
-                                <td>
-                                    client312402@example.com
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    789-654987
-                                </td>
-                                <td>
-                                    اسم العميل
-                                </td>
-                                <td>
-                                    اسم العميل
-                                </td>
-                                <td>
-                                    client312402@example.com
-                                </td>
-                            </tr>
+                            <tbody>
+                                <tr>
+                                    <td style={{ display:  billId ? "table-cell" : "none"}}>
+                                        789-654987
+                                    </td>
+                                    <td style={{ display: clientName ? "table-cell" : "none"}}>
+                                        اسم العميل
+                                    </td>
+                                    <td style={{ display: clienLasttName ? "table-cell" : "none"}}>
+                                        اسم العميل
+                                    </td>
+                                    <td style={{ display: email ? "table-cell" : "none"}}>
+                                        client312402@example.com
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style={{ display:  billId ? "table-cell" : "none"}}>
+                                        789-654987
+                                    </td>
+                                    <td style={{ display: clientName ? "table-cell" : "none"}}>
+                                        اسم العميل
+                                    </td>
+                                    <td style={{ display: clienLasttName ? "table-cell" : "none"}}>
+                                        اسم العميل
+                                    </td>
+                                    <td style={{ display: email ? "table-cell" : "none"}}>
+                                        client312402@example.com
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style={{ display:  billId ? "table-cell" : "none"}}>
+                                        789-654987
+                                    </td>
+                                    <td style={{ display: clientName ? "table-cell" : "none"}}>
+                                        اسم العميل
+                                    </td>
+                                    <td style={{ display: clienLasttName ? "table-cell" : "none"}}>
+                                        اسم العميل
+                                    </td>
+                                    <td style={{ display: email ? "table-cell" : "none"}}>
+                                        client312402@example.com
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style={{ display:  billId ? "table-cell" : "none"}}>
+                                        789-654987
+                                    </td>
+                                    <td style={{ display: clientName ? "table-cell" : "none"}}>
+                                        اسم العميل
+                                    </td>
+                                    <td style={{ display: clienLasttName ? "table-cell" : "none"}}>
+                                        اسم العميل
+                                    </td>
+                                    <td style={{ display: email ? "table-cell" : "none"}}>
+                                        client312402@example.com
+                                    </td>
+                                </tr>
+                            </tbody>
                         </table>
                     </div>
                 </Card>
